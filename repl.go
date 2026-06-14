f12ba785888059b523f23fceb364948fd2687dad
jmiguel-hdez <jmiguel.hdez@gmail.com>
2026-06-14 09:58:38 -0500

    repl first version

--- repl.go
+++ repl.go
@@ -1,8 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	lowercase_text := strings.ToLower(text)
 	return strings.Fields(lowercase_text)
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var input string
+	var command string
+	for {
+		fmt.Print("Pokedex > ")
+		for scanner.Scan() {
+			input = scanner.Text()
+			words := cleanInput(input)
+			command = words[0]
+			fmt.Println("your command was: " + command)
+			break
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	}
 }
